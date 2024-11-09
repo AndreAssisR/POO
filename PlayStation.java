2446378be5862afc8d40fe4a815b5f7b5fb54bc0
AndreAssisR <131421357+AndreAssisR@users.noreply.github.com>
2024-11-09 15:12:13 -0300

    Update PlayStation.java

--- PlayStation.java
+++ PlayStation.java
@@ -1,1 +1,14 @@
+public class PlayStation extends Dispositivo
+{
+    private Manete Joogador1;
+    
+    public void botaoCinco(){
+        System.out.println("Canal Down");
+        estado--;
+    }
+    
+    public void botaoSeis(){
+        System.out.println("Canal UP"); estado++;
+    }
 
+}
