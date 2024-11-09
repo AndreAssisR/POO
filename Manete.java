f9454245c77c30cb516624da8d66fd7b11753d84
AndreAssisR <131421357+AndreAssisR@users.noreply.github.com>
2024-11-09 15:16:16 -0300

    Update Manete.java

--- Manete.java
+++ Manete.java
@@ -1,1 +1,23 @@
+public class Manete extends Dispositivo
+{
+    private boolean botao1, botao2, botao3;
+    
+    public void BotaoUm(){
+        botao1==true;
+        System.out.println("Pula");
+        botao1==false;
+    }
+    
+    public void BotaoUm(){
+        botao2==true;
+        System.out.println("Ataca");
+        botao2==false;
+    }
+  
+  public void BotaoTres(){
+        botao3==true;
+        System.out.println("Gira");
+        botao3==false;
+    }
 
+}
