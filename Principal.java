58416cd5aca76c5255904e8656d2ef38ece83e88
AndreAssisR <131421357+AndreAssisR@users.noreply.github.com>
2024-11-09 15:02:49 -0300

    Add files via upload

---
+++ Principal.java
@@ -0,0 +1,27 @@
+
+/**
+ * Escreva uma descrição da classe Principal aqui.
+ * 
+ * @author (seu nome) 
+ * @version (um número da versão ou uma data)
+ */
+public class Principal
+{
+
+    public static void main(String[] aaaaaaaaaaaaaaaaaaa){
+        ControleRemoto tv1 = new TvQueFicaMuda(new TV(1,100));
+        ControleRemoto tv2 = new TvQuePausa(new TV(1,200));
+    
+        System.out.println("Testando a TV que fica muda:");
+        tv1.botaoCinco();
+        tv1.botaoSeis();
+        tv1.botaoNove();
+        
+        System.out.println("Testando a TV que pausa:");
+        tv2.botaoCinco();
+        tv2.botaoSeis();
+        tv2.botaoNove();
+        
+    }
+
+}
